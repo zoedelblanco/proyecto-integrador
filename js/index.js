2c9722d937637f95c6e1ce2b06175f37349c4bb2
SerenaFabbian <sfabbian@udesa.edu.ar>
2023-06-05 13:59:22 -0300

    home

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let url = "https://api.deezer.com/artist/27/related"
+
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+
+        console.log(data);
+        let contenedor = document.querySelector(".articulos3");
+        let home =[];
+        
+        for (let i=0; i<5; i++){
+            home +=  `<ul>
+            <img src= "${data.data[i].picture}" alt='' />
+            <p>Name: ${data.data[i].name} </p>
+            <a href=""> Ir a detalle </a>
+        </ul>`
+
+        contenedor.innerHTML=home
+        }
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    })
