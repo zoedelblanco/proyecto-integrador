afae177f40ef34d82b73ad6b87b66b0f6ef6dce4
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2023-06-06 17:48:20 -0300

    artistas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //formulario//
-let formulario = document.querySelector(".formulario");
+/*let formulario = document.querySelector(".formulario");
 let buscar = document.querySelector('[name=buscador]')
 
 form.addEventListener('submit', function(e){
@@ -12,60 +12,35 @@ form.addEventListener('submit', function(e){
     } else {
         form.submit()
     }
-})
+}) */
 
 
 //artistas//
 let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists"
 
 fetch(url)
-    .then(function (response) {
+    .then(function(response) {
         return response.json()
     })
     .then(function (data) {
 
         console.log(data);
-        let contenedor = document.querySelector(".articulos3");
-        let home = '';
+
+        let contenedor = document.querySelector(".artistas");
+
+        let home = [];
         
         for (let i = 0; i < 5; i++){
-            home +=  `<article>
-            <img src= "${data.data[i].picture}" alt='' />
-            <p>Name: ${data.data[i].name} </p>
-            <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
-        </article>`
 
-        }
-        contenedor.innerHTML=home
-    })
-    .catch(function (error) {
-        console.log("Error: " + error);
-    })
-
-
-//canciones//
-let url2 = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/track"
-
-fetch(url)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-
-        console.log(data);
-        let contenedor = document.querySelector(".articulos1");
-        let home =[];
-        
-        for (let i=0; i<5; i++){
             home +=  `<ul>
-            <img src= "${data.data[i].id}" alt='' />
-            <p>Name: ${data.data[i].titel} </p>
-            <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
-        </ul>`
-
+                <img src= "${data.data[i].picture}" alt='' />
+                <p>Name: ${data.data[i].name} </p>
+                <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
+            </ul>`
         }
         contenedor.innerHTML=home
+
     })
-    .catch(function (error) {
-        console.log("Error: " + error);
+    .catch(function(error) {
+        console.log("Eroor: " + error);
     })
