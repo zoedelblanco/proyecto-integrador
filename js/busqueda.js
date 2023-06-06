90ade29aac0c1b1b18cf531a320fa4e429de03e1
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2023-06-06 15:27:05 -0300

    busqueda

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,7 +1,7 @@
 let buscar = busqueda.get('buscar')
 let resultados = document.querySelector('.resultados')
 let contenido = ''                                      
-let titulo = document.querySelector('.formulario')  
+let titulo = document.querySelector('.busqueda')  
 titulo.innerHTML += buscar        
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${buscar}`)
 .then(respuesta =>{
@@ -11,7 +11,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${bus
     for(let i=0; i < dataBusqueda.data.length; i++){  
         contenido += `<article class="contenidoResultados">
                         <p class="titulosCanciones"><a href="detallecancion.html?id=${dataBusqueda.data[i].id}"> ${dataBusqueda.data[i].title} </a></p>
-                    </article>`
+                     </article>`
    }
    resultados.innerHTML += contenido
    if(dataBusqueda.data.length == 0){
