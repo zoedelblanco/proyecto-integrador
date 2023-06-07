c89a1a76ce88ad424567d9c3666abd8797016f10
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2023-06-07 16:55:29 -0300

    home

--- js/index.js
+++ js/index.js
@@ -14,36 +14,9 @@ form.addEventListener('submit', function(e){
     }
 }) */
 
-//artistas//
-let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists"
-fetch(url)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function (data) {
-
-        console.log(data);
-        let contenedor = document.querySelector(".artistas");
-        let home = [];
-        
-        for (let i=0; i<5; i++){
-            home +=  `<ul>
-            <img src= "${data.data[i].picture}" alt='' />
-            <p>Name: ${data.data[i].name} </p>
-            <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
-        </ul>`
-
-        }
-        contenedor.innerHTML=home
-
-    })
-    .catch(function(error) {
-        console.log("Eroor: " + error);
-    })
-
 //canciones//
-let url2 = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/track"
-fetch(url)
+let web = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks"
+fetch(web)
     .then(function (response) {
         return response.json()
     })
@@ -52,24 +25,24 @@ fetch(url)
         console.log(data);
         let contenedor = document.querySelector(".canciones");
         let home = [];
-        
-        for (let i = 0; i < 5; i++){
-            home +=  `<ul>
-            <img src= "${data.data[i].picture}" alt='' />
-            <p>Name: ${data.data[i].name} </p>
-            <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
+
+        for (let i = 0; i < 5; i++) {
+            home += `<ul>
+            <img src= "${data.data[i].album.cover}" alt='' />
+            <p>Name: ${data.data[i].title} </p>
+            <a href="detalle-cancion.html?id=${data.data[i].id}">Ir a detalle </a>
         </ul>`
 
         }
-        contenedor.innerHTML=home
+        contenedor.innerHTML = home
     })
     .catch(function (error) {
         console.log("Error: " + error);
     })
 
 //albums//
-let url3 = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists"
-fetch(url3)
+let link = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/albums"
+fetch(link)
     .then(function (response) {
         return response.json()
     })
@@ -78,18 +51,44 @@ fetch(url3)
         console.log(data);
         let contenedor = document.querySelector(".albumes");
         let home = [];
-        
-        for (let i = 0; i < 5; i++){
-            home +=  `<ul>
+
+        for (let i = 0; i < 5; i++) {
+            home += `<ul>
+            <img src= "${data.data[i].cover}" alt='' />
+            <p>Name: ${data.data[i].title} </p>
+            <a href="detalle-album.html?id=${data.data[i].id}">Ir a detalle </a>
+        </ul>`
+
+        }
+        contenedor.innerHTML = home
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    })
+
+//artistas//
+let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists"
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+
+        console.log(data);
+        let contenedor = document.querySelector(".artistas");
+        let home = [];
+
+        for (let i = 0; i < 5; i++) {
+            home += `<ul>
             <img src= "${data.data[i].picture}" alt='' />
             <p>Name: ${data.data[i].name} </p>
             <a href="detalle-artista.html?id=${data.data[i].id}">Ir a detalle </a>
         </ul>`
 
         }
-        contenedor.innerHTML=home
+        contenedor.innerHTML = home
+
     })
     .catch(function (error) {
         console.log("Error: " + error);
     })
-
