e55c8f87e9246115f8acfdca318b8ac835ab1431
SerenaFabbian <sfabbian@udesa.edu.ar>
2023-06-09 12:54:19 -0300

    detalle artista

---
+++ js/detalle-artista.js
@@ -0,0 +1,27 @@
+let queryString = location.search
+let objetoId = new URLSearchParams(queryString);
+let id = objetoId.get("id");
+
+let detalle = document.querySelector(".detalleArtista");
+let contenido = " ";
+
+let url =  `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artists/${id}`
+
+fetch(url)
+    .then(function (response) {
+    return response.json()
+    })
+    .then(function (data) {
+    console.log(data);
+
+    contenido += `<article class="detalleArtista">
+    <img src="${data.picture}" alt="">
+    <p>${data.name}</p>
+    <ul>${data.nb_album}</ul>
+    </article>`
+
+    detalle.innerHTML = contenido
+})
+.catch(function (error) {
+    console.log("Error: " + error);
+})
