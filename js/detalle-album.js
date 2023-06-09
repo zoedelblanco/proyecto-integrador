69c20d70cc6274ec4b6bc26114c705ceef70b33d
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2023-06-09 12:49:22 -0300

    detalle

--- js/detalle-album.js
+++ js/detalle-album.js
@@ -14,15 +14,25 @@ fetch(url)
    .then(function (data) {
       console.log(data);
       
-      contenido += `<article class="detalleAlbum">
-         <img src="${data.cover_medium} alt="">
-         <p>${data.title}</p>
-         <p>${data.artist.name}</p>
-         <p>${data.genres.name}</p>
-         <p>${data.release_date}</p>
-         </article>`
+      // contenido += `<article class="detalleAlbum">
+      //    <img src="${data.cover_medium}" alt="">
+      //    <p>${data.title}</p>
+      //    <p>${data.artist.name}</p>
+      //    <p>${data.genres.data[0].name}</p>
+      //    <p>${data.release_date}</p>
+      //    <p>${data.tracks.data.title}</p>
+      //    </article>`
 
-      detalle.innerHTML = contenido
+      // detalle.innerHTML = contenido
+
+      //1) capturar cada uno de los elementos del detalle y ponerles info desde js
+      let img = document.querySelector(".img");
+      img.src = data.cover_medium;
+      let title = document.querySelector(".title");
+      title.innerText = data.title
+
+      //2) Armar la lista de tracks
+
 
    })
    .catch(function (error) {
