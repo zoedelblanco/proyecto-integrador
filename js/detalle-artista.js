1eb9775f476e60a44f3a4e6d4be4c8c1dc880dd4
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2023-06-13 23:28:41 -0300

    h2

--- js/detalle-artista.js
+++ js/detalle-artista.js
@@ -21,6 +21,32 @@ fetch(url)
             </article>`
 
         detalle.innerHTML = contenido
+
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    })
+    
+    
+let listaAlbums = document.querySelector(".listaAlbums")
+contenidoAlbum = []
+
+let api= `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}/top?limit=50`
+
+fetch(api)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (i = 0; i < 5; i++) {
+
+            contenidoAlbum += `<article>${data.tracklist[i].data.album.title}</article>`;
+
+        }
+
+        listaAlbums.innerHTML = contenidoAlbum
     })
     .catch(function (error) {
         console.log("Error: " + error);
