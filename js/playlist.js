485b6b6b9193bcf5cdb8151c5959ba98cbe754a3
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2023-06-15 00:21:35 -0300

    playlist

---
+++ js/playlist.js
@@ -0,0 +1,17 @@
+/* 
+
+let play = document.querySelector(".playlist")
+
+let arrayPlay = JSON.parse(localStorage.getItem("playlist"))
+
+let contenido = []
+
+let arrayArtistas = data.data
+
+if (arrayPlay == null){
+    alert('La playlist esta vacia')
+}else{
+    for(i=0 , i<arrayPlay.length , i++)
+}
+
+ */
